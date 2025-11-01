989bd273da363209c81118e4dcebd14fee5027e2
yorumindd <yevhenii.rusnak@gene.co.uk>
2025-11-01 17:31:24 +0000

    Updated UITest file and ContentView file

---
+++ StarterKit/StarterKit/StarterKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarterKitApp.swift
+//  StarterKit
+//
+//  Created by Yevhenii Rusnak on 01.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct StarterKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StarterKit/StarterKitTests/StarterKitTests.swift
@@ -0,0 +1,15 @@
+//
+//  StarterKitTests.swift
+//  StarterKitTests
+//
+//  Created by Yevhenii Rusnak on 01.11.2025.
+//
+
+import XCTest
+
+final class StarterKitTests: XCTestCase {
+    func testExampleAlwaysTrue() throws {
+        XCTAssertTrue(true)
+    }
+}
+
